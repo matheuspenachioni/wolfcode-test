9a3e06069f393566bb81d245d33ec0a1fe2bb16f
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-05-09 12:56:29 -0300

    validação e criação de user

--- action/create_user.php
+++ action/create_user.php
@@ -6,21 +6,44 @@ require '../database_conn.php';
 $name = filter_input(INPUT_POST, 'name');
 
 // Varíavel email busca um input chamado EMAIL de um form POST
-$email = filter_input(INPUT_POST, 'email');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
 // Varíavel password busca um input chamado PASSWORD de um form POST
 $password = filter_input(INPUT_POST, 'password');
 
-/* Varíavel sql chama a varíavel pdo criada no arquivo de configuração
-e prepara uma query personalizada */
-$sql = $pdo->prepare("INSERT INTO users(name, email, password) VALUES(:name, :email, :password)");
+if($name && $email) {
 
-// Ligando os valores do formulário ao seus respectivos atributos
-$sql->bindValue(':name', $name);
-$sql->bindValue(':email', $email);
-$sql->bindValue(':password', $password);
+    /* Varíavel sql chama a varíavel pdo criada no arquivo de configuração
+    e prepara uma query para a consulta do email */ 
+    $sql = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $sql->bindValue(':email', $email);
+    $sql->execute();
 
-// Executa a query
-$sql->execute();
+    // Se não houver nenhuma coluna com o mesmo email
+    if($sql->rowCount() === 0) {
+        /* Varíavel sql chama a varíavel pdo criada no arquivo de configuração
+        e prepara uma query personalizada */
+        $sql = $pdo->prepare("INSERT INTO users(name, email, password) VALUES(:name, :email, :password)");
+    
+        // Ligando os valores do formulário ao seus respectivos atributos
+        $sql->bindValue(':name', $name);
+        $sql->bindValue(':email', $email);
+        $sql->bindValue(':password', $password);
+    
+        // Executa a query
+        $sql->execute();
+    
+        // Volta para a página de listagem
+        header("Location: ../index.php");
+        exit;
+    } else {
+        // Volta para a página de insert
+        header("Location: ../insert.php");
+    }
+} else {
+    // Volta para a página de insert
+    header("Location: ../insert.php");
+    exit;
+}
 
-header("Location: ../index.php");
+
