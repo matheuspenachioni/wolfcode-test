00737f7118cab3d098f6c12afbe818f6eea283cd
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-05-09 12:12:51 -0300

    tela de insert - parte 1

---
+++ insert.php
@@ -0,0 +1,16 @@
+<h1>Cadastrar Usuário</h1>
+<form action="" method="POST">
+    <div>
+        <label>Nome</label>
+        <input type="text" name="name"/>
+    </div>
+    <div>
+        <label>E-mail</label>
+        <input type="email" name="email"/>
+    </div>
+    <div>
+        <label>Senha</label>
+        <input type="password" name="password"/>
+    </div>
+    <input type="button" value="Salvar">
+</form>
