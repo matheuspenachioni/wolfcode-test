1bd90b4ba73b1827899c217d6c063242207b8bfa
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-05-09 14:48:05 -0300

    layout minimalista com bootstrap

--- index.php
+++ index.php
@@ -11,31 +11,57 @@
         $list = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
-<h1>Listagem de Usuários</h1>
-<a href="insert.php">Novo usuário</a>
+<div class="container">
+    <h1 class="text-center">Listagem de Usuários</h1>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <a href="insert.php" class="btn btn-primary">Novo usuário</a>
+    </div>
 
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>E-mail</th>
-        <th>Ações</th>
-    </tr>
-    <!--- foreach de list em user -->
-    <?php foreach($list as $user) :?>
+    <table class="table table-striped mt-4">
         <tr>
-            <!-- Informamos qual campo queremos de user -->
-            <td><?=$user['id'];?></td>
-            <td><?=$user['name'];?></td>
-            <td><?=$user['email'];?></td>
-            <!-- Achei melhor não mostrar a senha por questões de segurança -->
-            <!-- <td><?=$user['password'];?></td> -->
-            <!-- Passa um parâmetro na URL do ID de user para saber quem estamos editando -->
-            <td><a href="edit.php?id=<?=$user['id'];?>"><i class="fal fa-edit"></i> Editar</a></td>
-            <!-- Passa um parâmetro na URL do ID de user para saber quem estamos deletando -->
-            <td><a href="delete.php?id=<?=$user['id'];?>"><i class="fal fa-trash"></i> Excluir</a></td>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th colspan="2">Ações</th>
         </tr>
-    <?php endforeach; ?>
-</table>
+        <!--- foreach de list em user -->
+        <?php foreach($list as $user) :?>
+            <tr>
+                <!-- Informamos qual campo queremos de user -->
+                <td><?=$user['id'];?></td>
+                <td><?=$user['name'];?></td>
+                <td><?=$user['email'];?></td>
+                <!-- Achei melhor não mostrar a senha por questões de segurança -->
+                <!-- <td><?=$user['password'];?></td> -->
+                <!-- Passa um parâmetro na URL do ID de user para saber quem estamos editando -->
+                <td><a href="edit.php?id=<?=$user['id'];?>" class="btn btn-primary"><i class="fal fa-edit"></i> Editar</a></td>
+                <!-- Passa um parâmetro na URL do ID de user para saber quem estamos deletando -->
+                <td><a class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteDialog<?=$user['id'];?>"><i class="fal fa-trash"></i> Excluir</a></td>
+            </tr>
+
+            <!-- Dialog de confirmação -->
+            <div class="modal fade" id="deleteDialog<?=$user['id'];?>" tabindex="-1" aria-labelledby="deleteDialogLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="deleteDialogLabel">Confirme para prosseguir</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Tem certeza que deseja excluir o usuário <?=$user['name'];?>?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <a href="delete.php?id=<?=$user['id'];?>" class="btn btn-danger">Excluir</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </table>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
--- insert.php
+++ insert.php
@@ -1,16 +1,26 @@
-<h1>Cadastrar Usuário</h1>
-<form action="action/create_user.php" method="POST">
-    <div>
-        <label>Nome</label>
-        <input type="text" name="name"/>
-    </div>
-    <div>
-        <label>E-mail</label>
-        <input type="email" name="email"/>
-    </div>
-    <div>
-        <label>Senha</label>
-        <input type="password" name="password"/>
-    </div>
-    <input type="submit" value="Salvar">
-</form>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+<div class="container">
+    <h1 class="text-center">Cadastrar Usuário</h1>
+    <form action="action/create_user.php" method="POST">
+        <div class="mb-3">
+            <label class="form-label fw-bold">Nome</label>
+            <input type="text" name="name" class="form-control"/>
+        </div>
+        <div class="mb-3">
+            <label class="form-label fw-bold">E-mail</label>
+            <input type="email" name="email" class="form-control"/>
+        </div>
+        <div class="mb-3">
+            <label class="form-label fw-bold">Senha</label>
+            <input type="password" name="password" class="form-control"/>
+        </div>
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <button type="submit" value="Salvar" class="btn btn-primary"><i class="fal fa-save"></i> Salvar</button>
+            <a href="index.php" class="btn btn-danger"><i class="fal fa-arrow-left"></i> Voltar</a>
+        </div>
+    </form>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
